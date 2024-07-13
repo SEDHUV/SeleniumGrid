514cab9541db33b954fd36678e1be991d02a87b0
SEDHUV <sedhuraina968@gmail.com>
2024-07-13 13:11:01 +0530

    commit

---
+++ untitled/src/test/java/grid/chrome_google.java
@@ -0,0 +1,27 @@
+package grid;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class chrome_google {
+
+        @Test
+        public void googleTest() throws MalformedURLException {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability(CapabilityType.BROWSER_NAME,"chrome");
+            WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.34:4444"),caps);
+            driver.get("https://www.google.co.in/");
+            String title = driver.getTitle();
+            System.out.println(title);
+            driver.quit();
+        }
+
+    }
+
+
